d5710e6d46393e301b06cc931d30058aeeca0e97
Arshia Naseri <36360796+arshia-naseri@users.noreply.github.com>
2021-06-25 20:27:24 -0400

    sec try

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import contact from './contact.vcf'
+
+function App() {
+
+  useEffect(()=>{
+    autoClick()
+  },[])
+
+  function autoClick(){
+    const btnDownload = document.getElementById('btnDownload')
+    // btnDownload.click()
+  }
+
+  return (
+    <>
+    <a id={'btnDownload'} href={contact} download>This is the page</a>
+    </>
+  )
+}
+
+
+export default App;
